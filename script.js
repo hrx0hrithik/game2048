730e2ecdb71801001ee29e324f7208469d2a1f52
hrx0hrithik <hrithikgupta52@gmail.com>
2023-04-30 15:46:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 console.log(grid.randomEmptyCell())
 
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
